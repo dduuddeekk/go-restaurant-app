3d458cad6b20da1027d96433e1bddcfb7458d8b4
dduuddeekk <jajajablablabla123@gmail.com>
2025-01-02 16:19:06 +0800

    Connecting it to pgsql.

--- main.go
+++ main.go
@@ -1,57 +1,109 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var dbAddress string
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	dbAddress = "host=" + os.Getenv("DB_HOST") +
+		" port=" + os.Getenv("DB_PORT") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" sslmode=" + os.Getenv("DB_SSLMODE")
+}
+
+const (
+	MenuTypeFood  = "food"
+	MenuTypeDrink = "drink"
+)
+
+type MenuType string
 
 type MenuItem struct {
 	Name      string
 	OrderCode string
 	Price     int
+	Type      MenuType
 }
 
-func getFoodMenu(c echo.Context) error {
+func seedDB() {
 	foodMenu := []MenuItem{
 		{
 			Name:      "Nasi Goreng",
 			OrderCode: "nasigoreng",
 			Price:     15000,
+			Type:      MenuTypeFood,
 		},
 		{
 			Name:      "Babi Guling",
 			OrderCode: "babiguling",
 			Price:     30000,
+			Type:      MenuTypeFood,
 		},
 	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"data": foodMenu,
-	})
-}
 
-func getDrinkMenu(c echo.Context) error {
 	drinkMenu := []MenuItem{
 		{
 			Name:      "Arak Bali",
 			OrderCode: "arakbali",
 			Price:     14000,
+			Type:      MenuTypeDrink,
 		},
 		{
 			Name:      "Es Teh",
 			OrderCode: "esteh",
 			Price:     3000,
+			Type:      MenuTypeDrink,
 		},
 	}
+
+	db, err := gorm.Open(postgres.Open(dbAddress))
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&MenuItem{})
+
+	if err := db.First(&MenuItem{}).Error; err == gorm.ErrRecordNotFound {
+		db.Create(&foodMenu)
+		db.Create(&drinkMenu)
+	}
+}
+
+func getMenu(c echo.Context) error {
+	menuType := c.FormValue("type")
+
+	db, err := gorm.Open(postgres.Open(dbAddress))
+	if err != nil {
+		panic(err)
+	}
+
+	var menuData []MenuItem
+
+	db.Where(MenuItem{Type: MenuType(menuType)}).Find(&menuData)
+
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"data": drinkMenu,
+		"data": menuData,
 	})
 }
 
 func main() {
+	seedDB()
 	e := echo.New()
-	// localhost:14045/menu/food
-	e.GET("/menu/food", getFoodMenu)
-	e.GET("/menu/drink", getDrinkMenu)
+	e.GET("/menu", getMenu)
 	e.Logger.Fatal(e.Start(":14045"))
 }
