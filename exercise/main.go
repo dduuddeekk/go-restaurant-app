626392177febc6a59fc3366bdd9eb35911847024
dduuddeekk <jajajablablabla123@gmail.com>
2025-01-20 14:34:06 +0800

    ignore

---
+++ exercise/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func testPrint(id int) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("[%d] counting %d\n", id, i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i < 3; i++ {
+// 		wg.Add(1)
+// 		go func(i int) {
+// 			defer wg.Done()
+// 			testPrint(i)
+// 		}(i)
+// 	}
+
+// 	wg.Wait()
+// }
+
+func main() {
+	var sharedResource string
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			mutex.Lock()
+			fmt.Println("Previous value: ", sharedResource)
+			sharedResource = fmt.Sprintf("key owned by: [%d]", id)
+			fmt.Println("Current value: ", sharedResource)
+			mutex.Unlock()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("final resource: ", sharedResource)
+}
